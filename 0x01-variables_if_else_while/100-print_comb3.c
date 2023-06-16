d7143e260b7e0fac11c2a6ebdf5f4d02ce1e55a8
Mohamed Kartit <mkartit9@gmail.com>
2023-06-16 12:04:43 -0700

    Add the 100-print_comb3 file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/* 10. Inventing is a combination of brains and materials.
+ * The more brains you use, the less material you need */
+
+/**
+ * main - prints all possible different combinations of two digits.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n , i;
+
+	for (n = 48; n <= 56; n++)
+	{
+		for (i = 49; i <= 57; i++)
+		{
+			if ( i > n)
+			{
+
+				putchar(n);
+				putchar(i);
+				if (n != 56 || i != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
