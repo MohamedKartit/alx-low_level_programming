6f6664d51e674cd8053fef2fb48db0b527935f24
Mohamed Kartit <mkartit9@gmail.com>
2023-06-16 05:26:39 -0700

    Add the 3-print-alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/*
+ * 3. alphABET
+ */
+
+/**
+ * main - prints the alphabets lower and upper cases
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int n = 97;
+	int u = 65; 
+
+	while (n <= 122)
+	{
+		char i = (char)n;
+
+		putchar(i);
+		n++;
+	}
+	while (u <= 90)
+	{
+		char i = (char)u;
+
+		putchar(i);
+		u++;
+	}
+	putchar(10);
+
+	return (0);
+}
