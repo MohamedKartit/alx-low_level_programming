deeb373d4ef088df92ce7c4c6b819fe8cd857bcb
Mohamed Kartit <mkartit9@gmail.com>
2023-06-16 12:08:23 -0700

    Update 100-print_comb.c file fix the spacing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-/* 10. Inventing is a combination of brains and materials.
- * The more brains you use, the less material you need */
+/*
+ * 10. Inventing is a combination of brains and materials.
+ * The more brains you use, the less material you need
+ */
 
 /**
  * main - prints all possible different combinations of two digits.
@@ -11,13 +13,13 @@
 
 int main(void)
 {
-	int n , i;
+	int n, i;
 
 	for (n = 48; n <= 56; n++)
 	{
 		for (i = 49; i <= 57; i++)
 		{
-			if ( i > n)
+			if (i > n)
 			{
 
 				putchar(n);
