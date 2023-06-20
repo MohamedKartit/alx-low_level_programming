24cd7ce90f6c7024d3f38fe3f2c07efa4baf4ce9
Mohamed Kartit <mkartit9@gmail.com>
2023-06-20 05:18:03 -0700

    Add the 1-alphabet.c file that prints alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  prints the alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+}
