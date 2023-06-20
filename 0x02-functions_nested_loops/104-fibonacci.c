515e2354012cbe9582e9e67af2fcfe4dab74a36c
Mohamed Kartit <mkartit9@gmail.com>
2023-06-20 15:13:05 -0700

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,20 +9,33 @@
 
 int main(void)
 {
-	unsigned long int prev, curr, next;
+	unsigned long int prev, curr, n, n1, k, k1;
 	int i;
 
 	prev = 1;
 	curr = 2;
 
-	printf("%lu, %lu", prev, curr);
-	for (i = 3; i <= 98; i++)
+	printf("%lu", prev);
+	for (i = 1; i < 91; i++)
 	{
-		next = prev + curr;
-		printf(", %lu", next);
+		printf(", %lu", curr);
+		curr = curr + prev;
+		prev = curr - prev;
+	}
+	n = prev / 1000000000;
+	n1 = prev % 1000000000;
+	k = curr / 1000000000;
+	k1 = curr % 1000000000;
 
-		prev = curr;
-		curr = next;
+	for (i = 92; i < 99; i++)
+	{
+		printf(", %lu", k + (k1 / 1000000000));
+		printf("%lu", k1 % 1000000000);
+
+		k = k + n;
+		n = k - n;
+		k1 = k1 + n1;
+		n1 = k1 - n1;
 	}
 	printf("\n");
 
