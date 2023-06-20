0b2dfd6c1b202892837a91dc48e4c69d8aaa4a50
Mohamed Kartit <mkartit9@gmail.com>
2023-06-20 11:39:05 -0700

    Add the 10-add.c file that add 2 integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - add 2 integers
+ *
+ * @a: integer to be added to b
+ * @b: integer to be added to a
+ *
+ * Return: returns the sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
