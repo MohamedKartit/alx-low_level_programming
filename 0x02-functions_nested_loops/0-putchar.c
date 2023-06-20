865e7244df0c0c5d8ec140eb0fd66e65a1299d4e
Mohamed Kartit <mkartit9@gmail.com>
2023-06-20 04:20:14 -0700

    Add th main.c, _putchar.c and 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - This function prints _putchar followedby new line
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
