4dbe63fc33ab72374a354d171cbcb621cf9504a5
Mohamed Kartit <mkartit9@gmail.com>
2023-06-20 10:29:42 -0700

    Update the file 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,9 +13,16 @@ int print_last_digit(int n)
 	int Last_Num;
 
 	if (n < 0)
+	{
 		n = -n;
-
+	}
+	
 	Last_Num = n % 10;
+	if (Last_Num < 0)
+	{
+		Last_Num = -Last_Num;
+	}
 	_putchar('0' + Last_Num);
+
 	return (Last_Num);
 }
