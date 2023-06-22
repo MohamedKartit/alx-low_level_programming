0d83f77e6029299e4ecd57a60d29f831fe04525e
Mohamed Kartit <mkartit9@gmail.com>
2023-06-22 05:16:23 -0700

    Add the 9-fizz_buzz.c that does the fizz buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Fizz-Buzz test
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", n);
+		if (n != 100)
+			printf(" ");
+		n++;
+	}
+	printf("\n");
+
+	return (0);
+}
