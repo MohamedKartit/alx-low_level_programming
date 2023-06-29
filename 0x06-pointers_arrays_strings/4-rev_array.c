2093531ef7a09c946420e17806eb9c1c2c20c4c9
Mohamed Kartit <mkartit9@gmail.com>
2023-06-29 10:47:21 -0700

    Add the 4-rev_array.c file

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - reverses an array of integers
+ * @a: array to be reversed
+ * @n: number of elements in the array
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, tmp;
+
+	i = 0;
+	j = n - 1;
+
+	while (i < n / 2)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j--] = tmp;
+		i++;
+	}
+}
