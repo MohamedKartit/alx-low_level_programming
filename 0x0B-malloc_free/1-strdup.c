2d0fe12d9731ae7a67c39982968a543d5a296c6c
Mohamed Kartit <mkartit9@gmail.com>
2023-07-10 04:26:06 -0700

    update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,25 +13,28 @@
 char *_strdup(char *str)
 {
 	char *ptr;
+	unsigned int len, i;
 
-	int len = 0;
-	int i = 0;
-
-	while (str[len] != '\0')
-		len++;
-
-	ptr = (char *) malloc(sizeof(char) * len);
+	len = 0;
+	i = 0;
 
 	if (str == NULL)
 		return (NULL);
 
+	while (str[len])
+		len++;
+
+	ptr = (char *) malloc(sizeof(char) * (len + 1));
+
+	if (ptr == NULL)
+		return (NULL);
+
 	while (i < len)
 	{
-		if (ptr == NULL)
-			return (NULL);
 		*(ptr + i) = *(str + i);
 		i++;
 	}
+
 	*(ptr + i) = '\0';
 
 	return (ptr);
