f8fa3f97e245143d3f3b1cb609c595d2ed78118f
Mohamed Kartit <mkartit9@gmail.com>
2023-08-03 11:19:01 -0700

    Add the 4-clear_bit.c file that sets value of a bit to 0 at a given index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * clear_bit - set the value of a bit to 0 at a given index.
+ *
+ * @n: pointer to decimal number to change
+ * @index: index position to change
+ *
+ * Return: 1 success, -1 error
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i = 0;
+	unsigned int x;
+
+	if (index > 64)
+		return (-1);
+
+	x = index;
+
+	while (x > 0)
+	{
+		i *= 2;
+		x--;
+	}
+
+	if ((*n >> index) & 1)
+		*n -= i;
+
+	return (1);
+}
